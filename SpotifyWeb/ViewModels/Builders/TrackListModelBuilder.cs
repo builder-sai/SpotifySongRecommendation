c7fc5c2d5ba5db11ac6c6ccb3dd780a3fd1fbada
Sai <saifalzobaydee@hotmail.com>
2018-01-15 10:27:53 +0100

    Moved spotify client to the construct

--- SpotifyWeb/ViewModels/Builders/TrackListModelBuilder.cs
+++ SpotifyWeb/ViewModels/Builders/TrackListModelBuilder.cs
@@ -10,22 +10,24 @@ namespace SpotifyWeb.ViewModels.Builders
     public class TrackListModelBuilder
     {
         private readonly RecommendationModel _recommendationModel;
+	    private readonly SpotifyApiClient _client;
         public TrackListModelBuilder(RecommendationModel recommendationModel)
         {
             _recommendationModel = recommendationModel;
+	        _client = new SpotifyApiClient();
         }
 
         public async Task<List<TrackViewModel>> BuildAsync()
         {
-            var client = new SpotifyApiClient();
-            var response = await client.SearchArtistsAsync(_recommendationModel.Artist);
+            
+            var response = await _client.SearchArtistsAsync(_recommendationModel.Artist);
             if (response.Artists != null && response.Artists.Total > 0)
             {
                 var artist = response.Artists.Items[0];
                 var danceability = _recommendationModel.Danceability ? 1.0f : 0.1f;
                 var energy = _recommendationModel.Energy * 0.1f;
                 var tempo = _recommendationModel.Tempo * 0.1f;
-                var trackResponse = await client.GetRecommendationsAsync(artist.Id, danceability, energy, tempo);
+                var trackResponse = await _client.GetRecommendationsAsync(artist.Id, danceability, energy, tempo);
 				//taking only these properties for now
                 return trackResponse?.Tracks.Select(t => new TrackViewModel {ArtistName = t.Artists?.FirstOrDefault()?.Name, TrackName = t.Name , TrackPreviewUrl = t.PreviewUrl}).ToList();
             }
